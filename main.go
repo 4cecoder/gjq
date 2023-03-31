0018625a677b90c6336e66a6074e10b68ac33236
4ce <88108711+4cecoder@users.noreply.github.com>
2023-03-31 00:28:53 -0400

    Update main.go
     removed unused flags, structured the code into separate functions, and added error handling

--- main.go
+++ main.go
@@ -5,28 +5,18 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
-	"github.com/fatih/color"
-	"regexp"
 	"os"
+	"regexp"
+
+	"github.com/fatih/color"
 )
 
 func main() {
 	compact := flag.Bool("c", false, "compact instead of pretty-printed output")
 	nullInput := flag.Bool("n", false, "use 'null' as the single input value")
 	exitStatus := flag.Bool("e", false, "set the exit status code based on the output")
-	slurp := flag.Bool("s", false, "read (slurp) all inputs into an array; apply filter to it")
-	rawStrings := flag.Bool("r", false, "output raw strings, not JSON texts")
-	rawInput := flag.Bool("R", false, "read raw strings, not JSON texts")
 	colorize := flag.Bool("C", false, "colorize JSON")
 	monochrome := flag.Bool("M", false, "monochrome (don't colorize JSON)")
-	sortKeys := flag.Bool("S", false, "sort keys of objects on output")
-	useTabs := flag.Bool("tab", false, "use tabs for indentation")
-	argValues := flag.String("arg", "", "set variable $a to value <v>")
-	argJSONValues := flag.String("argjson", "", "set variable $a to JSON value <v>")
-	slurpFile := flag.String("slurpfile", "", "set variable $a to an array of JSON texts read from <f>")
-	rawFile := flag.String("rawfile", "", "set variable $a to a string consisting of the contents of <f>")
-	args := flag.Bool("args", false, "remaining arguments are string arguments, not files")
-	jsonArgs := flag.Bool("jsonargs", false, "remaining arguments are JSON arguments, not files")
 	flag.Parse()
 
 	var input []byte
@@ -34,11 +24,82 @@ func main() {
 
 	if *nullInput {
 		input = []byte("null")
-	} else if *args {
-		input = []byte(flag.Arg(0))
-	} else if *jsonArgs {
-		input = []byte(flag.Arg(0))
 	} else if flag.NArg() > 0 {
+		input, err = ioutil.ReadFile(flag.Arg(0))
+		if err != nil {
+			exitWithError(err)
+		}
+	} else {
+		input, err = ioutil.ReadAll(os.Stdin)
+		if err != nil {
+			exitWithError(err)
+		}
+	}
+
+	var v interface{}
+	err = json.Unmarshal(input, &v)
+	if err != nil {
+		exitWithError(err)
+	}
+
+	var output []byte
+	if *compact {
+		output, err = json.Marshal(v)
+	} else {
+		output, err = json.MarshalIndent(v, "", "    ")
+	}
+	if err != nil {
+		exitWithError(err)
+	}
+
+	if *colorize {
+		output, err = colorizeJSON(output)
+		if err != nil {
+			exitWithError(err)
+		}
+	}
+
+	if *monochrome {
+		output = removeColorCodes(output)
+	}
+
+	fmt.Println(string(output))
+
+	if *exitStatus {
+		os.Exit(1)
+	}
+}
+
+func exitWithError(err error) {
+	fmt.Fprintln(os.Stderr, err)
+	os.Exit(1)
+}
+
+func colorizeJSON(jsonBytes []byte) ([]byte, error) {
+	var obj interface{}
+	err := json.Unmarshal(jsonBytes, &obj)
+	if err != nil {
+		return nil, err
+	}
+
+	colorized, err := json.MarshalIndent(obj, "", "    ")
+	if err != nil {
+		return nil, err
+	}
+
+	outputString := string(colorized)
+	outputString = color.GreenString(outputString)
+	outputString = color.BlueString(outputString)
+
+	return []byte(outputString), nil
+}
+
+func removeColorCodes(jsonBytes []byte) []byte {
+	re := regexp.MustCompile("\x1b\\[[0-9;]*[m|K]")
+	outputString := re.ReplaceAllString(string(jsonBytes), "")
+	return []byte(outputString)
+}
+
 		input, err = ioutil.ReadFile(flag.Arg(0))
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
